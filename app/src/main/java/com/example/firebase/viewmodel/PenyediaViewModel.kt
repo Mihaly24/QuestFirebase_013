3ac91536b24c19a36adae061bc58f94a78c85861
Mihaly24 <fairuzk20@gmail.com>
2026-01-09 19:00:36 +0700

    Menambah initializer DetailViewModel di file PenyediaViewModel

--- app/src/main/java/com/example/firebase/viewmodel/PenyediaViewModel.kt
+++ app/src/main/java/com/example/firebase/viewmodel/PenyediaViewModel.kt
@@ -1,6 +1,7 @@
 package com.example.firebase.viewmodel
 
 import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.createSavedStateHandle
 import androidx.lifecycle.viewmodel.CreationExtras
 import androidx.lifecycle.viewmodel.initializer
 import androidx.lifecycle.viewmodel.viewModelFactory
@@ -15,5 +16,11 @@ object PenyediaViewModel {
     val Factory = viewModelFactory {
         initializer { HomeViewModel(aplikasiDataSiswa().container.repositorySiswa) }
         initializer { EntryViewModel(aplikasiDataSiswa().container.repositorySiswa) }
+        initializer {
+            DetailViewModel(
+                createSavedStateHandle(),
+                aplikasiDataSiswa().container.repositorySiswa
+            )
+        }
     }
 }
